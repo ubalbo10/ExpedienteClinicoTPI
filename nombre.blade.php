bbef23dfeb3109b018a0aa7185997643f95762ca
ubalbo10 <guillememo95@gmail.com>
2019-11-06 23:28:04 -0600

    creando esqueleto de agenda de medico

---
+++ nombre.blade.php
@@ -0,0 +1,5 @@
+este archivo solo es creado para guiarnos como nombramos las plantillas en BLADE el gestor de vistas
+en laravel , lo cual es html mas uso de cosas propias de laravel
+
+@ llamamos cosas del lenguaje y de laravel
+{{ $variable  }}  con corchetes podemos ocupar variables nombradas por nosotros en el proyecto
