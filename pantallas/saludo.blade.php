807eaf99ec67374839acf4adf67c0cece79fdced
guillermo <guillememo95@gmail.com>
2019-11-11 10:08:42 -0600

    agregando registro medico

---
+++ pantallas/saludo.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Vista ejemplo</title>
+</head>
+<body>
+    <h1>Hola mundo desde Laravel</h1>
+</body>
+</html>
